33c9bb644912e8a900cc51d83a5f35d10feafc26
mclacore <michael@massdriver.cloud>
2023-03-02 15:16:21 -0700

    Refactoring inputs

--- calculator/calc.py
+++ calculator/calc.py
@@ -1,36 +1,30 @@
 # make a calculator app
+# solve taking in an entire string of numbers and operators and returning the answer
 
 operation = input("Which operation would you like to perform? (add, subtract, multiply, divide): ")
 options = ["add", "subtract", "multiply", "divide"]
 
-
-if operation == "add":
+def main():
     input_1 = input("Enter a number: ")
     input_2 = input("Enter another number: ")
 
-    answer = int(input_1) + int(input_2)
-    print(answer)
+    if operation == "add":
+        answer = int(input_1) + int(input_2)
+        print(answer)
 
 
-if operation == "subtract":
-    input_3 = input("Enter a number: ")
-    input_4 = input("Enter another number: ")
-
-    answer = int(input_3) - int(input_4)
-    print(answer)
+    if operation == "subtract":
+        answer = int(input_1) - int(input_2)
+        print(answer)
 
 
-if operation == "multiply":
-    input_5 = input("Enter a number: ")
-    input_6 = input("Enter another number: ")
-
-    answer = int(input_5) * int(input_6)
-    print(answer)
+    if operation == "multiply":
+        answer = int(input_1) * int(input_2)
+        print(answer)
 
 
-if operation == "divide":
-    input_7 = input("Enter a number: ")
-    input_8 = input("Enter another number: ")
+    if operation == "divide":
+        answer = int(input_1) / int(input_2)
+        print(answer)
 
-    answer = int(input_7) / int(input_8)
-    print(answer)
+main()
