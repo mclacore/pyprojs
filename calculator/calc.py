9dddaa897f45285894ec50376bca8c99770c4ea4
mclacore <michael@massdriver.cloud>
2023-03-02 15:21:02 -0700

    PEP8 formatting

--- calculator/calc.py
+++ calculator/calc.py
@@ -1,8 +1,10 @@
 # make a calculator app
 # solve taking in an entire string of numbers and operators and returning the answer
 
-operation = input("Which operation would you like to perform? (add, subtract, multiply, divide): ")
+operation = input(
+    "Which operation would you like to perform? (add, subtract, multiply, divide): ")
 options = ["add", "subtract", "multiply", "divide"]
+
 
 def main():
     input_1 = input("Enter a number: ")
@@ -11,18 +13,16 @@ def main():
     if operation == "add":
         answer = int(input_1) + int(input_2)
 
-
     if operation == "subtract":
         answer = int(input_1) - int(input_2)
-
 
     if operation == "multiply":
         answer = int(input_1) * int(input_2)
 
-
     if operation == "divide":
         answer = int(input_1) / int(input_2)
-        
+
     print(answer)
+
 
 main()
